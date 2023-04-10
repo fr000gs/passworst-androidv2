de6325149357c801b72e066f1a0f7e1c42be8993
Fat frog <gr340wl@gmail.com>
2023-04-10 16:11:22 +0530

    Add rust

--- passworstlib/compile.sh
+++ passworstlib/compile.sh
@@ -9,3 +9,7 @@ cargo build -j12 --release --target x86_64-linux-android
 cargo build -j12 --release --target armv7-linux-androideabi
 cargo build -j12 --release --target i686-linux-android
 
+cp target/aarch64-linux-android/release/passworstlib.so app/src/main/jniLibs/arm64-v8a
+cp target/armv7-linux-androideabi/release/passworstlib.so app/src/main/jniLibs/armeabi-v7a
+cp target/x86_64-linux-android/release/passworstlib.so app/src/main/jniLibs/x86_64
+cp target/i686-linux-android/release/passworstlib.so app/src/main/jniLibs/x86
